0062ba27b75d204d53288376254d1b2e2c174bfe
OtavioCunha-prog <otavio.filho.55070@a.fecaf.com.br>
2024-10-30 18:14:03 -0300

    Update Biblioteca.java

--- src/Biblioteca/Biblioteca.java
+++ src/Biblioteca/Biblioteca.java
@@ -7,9 +7,9 @@ public class Biblioteca {
 
 // Criação de objetos Livro com título, autor e preço
 
-        Livro livro1 = new Livro("Entenda Algoritmos", "O Programador Pragmático: De Aprendiz a Mestre", 45.00);
-        Livro livro2 = new Livro("Análise de Tráfego em Redes TCP/IP: Utilize Tcpdump na Análise de Tráfegos em Qualquer Sistema Operacional", "Avaliação De Segurança De Redes", 75.00);
-        Livro livro3 = new Livro("Arquitetura De Redes De Computadores", "Redes De Computadores - Use A Cabeça!", 120.00);
+        Livro livro1 = new Livro("Avaliação De Segurança De Redes", "Chris McNab", 45.00);
+        Livro livro2 = new Livro("Análise de Tráfego em Redes TCP/IP: Utilize Tcpdump na Análise de Tráfegos em Qualquer Sistema Operacional", "João Eriberto Mota Filho", 75.00);
+        Livro livro3 = new Livro("Arquitetura De Redes De Computadores", "Maia", 120.00);
 
         Cliente cliente = new Cliente("Otávio Alvernaz");
 
